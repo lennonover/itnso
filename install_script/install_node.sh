5447ba6e1f11b803753ff39d3c5038b1097cf556
walei <lennonover@gmail.com>
2018-01-29 20:05:38 +0800

    :bug: change install_node.sh

--- install_script/install_node.sh
+++ install_script/install_node.sh
@@ -1,5 +1,5 @@
 cd ../install_pack
 tar -xJf node-v8.9.4-linux-x64.tar.xz
-sudo mv node-v8.9.4-linux-x64.tar /opt/
-sudo ln -s /opt/node-v8.9.4-linux-x64.tar/bin/node /usr/local/bin/node
-sudo ln -s /opt/node-v8.9.4-linux-x64.tar/bin/npm /usr/local/bin/npm
+sudo mv node-v8.9.4-linux-x64 /opt/
+sudo ln -s /opt/node-v8.9.4-linux-x64/bin/node /usr/local/bin/node
+sudo ln -s /opt/node-v8.9.4-linux-x64/bin/npm /usr/local/bin/npm
